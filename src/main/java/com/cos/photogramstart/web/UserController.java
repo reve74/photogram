81e49b640e7ef6f340a234a1484c03538fd04f58
reve74 <reve99@naver.com>
2021-08-06 02:13:25 +0900

    세션정보

---
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -0,0 +1,31 @@
+package com.cos.photogramstart.web;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/user/{id}")
+	public String profile(@PathVariable int id) {
+		return "user/profile";
+	}
+	
+	@GetMapping("/user/{id}/update")
+	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+		System.out.println("세션 정보 : " + principalDetails.getUser());
+		
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		PrincipalDetails mPrincipalDetails = (PrincipalDetails)auth.getPrincipal();
+		System.out.println("직접찾은 세션정보" +mPrincipalDetails.getUser());
+		
+		return "user/update";
+	}
+}
--- src/main/webapp/WEB-INF/views/auth/signin.jsp
+++ src/main/webapp/WEB-INF/views/auth/signin.jsp
@@ -23,8 +23,8 @@
                    <div class="login__form">
                         <h1><img src="/images/logo.jpg" alt=""></h1>
                         
-                        <!--로그인 인풋-->
-                        <form class="login__input" >
+                        <!--로그인 인풋--> <!-- 로그인은 POST -->
+                        <form class="login__input"  action="/auth/signin" method="POST">
                             <input type="text" name="username" placeholder="유저네임" required="required" />
                             <input type="password" name="password" placeholder="비밀번호" required="required" />
                             <button>로그인</button>
