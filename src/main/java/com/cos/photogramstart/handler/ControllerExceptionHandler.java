4f6081163e368ad78fecba7ba9f77e5eef4995e7
reve74 <reve99@naver.com>
2021-08-05 23:38:29 +0900

    공통 응답 DTO

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -7,13 +7,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
+import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController
 @ControllerAdvice
 public class ControllerExceptionHandler {
 	
 	@ExceptionHandler(CustomValidationException.class)
-	public  Map<String, String> validationExceptoin(CustomValidationException e) {
-		return e.getErrorMap();
+	public  String validationExceptoin(CustomValidationException e) {
+		//CMRespDto, Script 비교
+		// 1. 클라이언트에게 응답할 때는 Script 좋음
+		// 2. Ajax통신 - CMResptDto
+		// 3. Android 통신 - CMRespDto
+		
+		return Script.back(e.getErrorMap().toString());
 	}
 }
+
+
